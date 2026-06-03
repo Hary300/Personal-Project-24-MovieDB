c2eed03082c159b728a2483f0ddf683f57e0b5b7
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-06-03 14:51:12 +0700

    add components and pages; implement layout structure; include Poppins font styles; add SVG icons

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import Hero from '../components/home/Hero';
+import MainLayout from '../layouts/MainLayout';
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <Hero />
+    </MainLayout>
+  );
+}
